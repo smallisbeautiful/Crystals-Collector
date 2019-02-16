42c38d1bcfbeed4aa95a0cf222a85989154ee409
Peggy Liu <peggyliu@Peggys-MacBook-Air-3.local>
2019-02-16 04:39:06 -0500

    Initial Site Files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,101 +1,79 @@
-$( document ).ready(function(){
-    // Generates random number to guess
-    var Random=Math.floor(Math.random()*102+19)
-    
-    // Display random number
-    $("#scoreToMatch").text(Random);
-    
-    //Generate random number for each crystal
-    var num1= Math.floor(Math.random()*12+1)
-    var num2= Math.floor(Math.random()*12+1)
-    var num3= Math.floor(Math.random()*12+1)
-    var num4= Math.floor(Math.random()*12+1)
-    
-    // Variables to keep track of wins, losses and total
-    var playerTotal= 0; 
-    var wins= 0;
-    var losses = 0;
-    
-  
-  $("#wins").text(wins);
-  $("#losses").text(losses);
-  
-  // Reset game
-  function reset(){
-        Random=Math.floor(Math.random()*102+19);
-        console.log(Random)
-        $("#scoreToMatch").text(Random);
-        num1= Math.floor(Math.random()*12+1);
-        num2= Math.floor(Math.random()*12+1);
-        num3= Math.floor(Math.random()*12+1);
-        num4= Math.floor(Math.random()*12+1);
-        playerTotal= 0;
-        $("#totalScore").text(playerTotal);
+$(document).ready(function() {
+	var randomNumber;
+	var totalScore = 0;
+	var wins = 0;
+	var losses = 0;
+	var redCrystalNum;
+	var yellowCrystalNum;
+	var blueCrystalNum;
+    var greenCrystalNum;
+
+	function newNumbers() {
+		randomNumber = Math.floor(Math.random() * 102) + 19;
+		redCrystalNum = Math.floor(Math.random() * 12) + 1;
+		yellowCrystalNum = Math.floor(Math.random() * 12) + 1;
+		blueCrystalNum = Math.floor(Math.random() * 12) + 1;
+		greenCrystalNum = Math.floor(Math.random() * 12) + 1;
+	}
+
+	function newGame() {
+		newNumbers();
+		totalScore = 0;
+		$("#randomNumber").text(randomNumber);
+		$("#totalScore").text(totalScore);
+		$("#redCrystal").attr("data-crystalvalue", redCrystalNum);
+		$("#yellowCrystal").attr("data-crystalvalue", yellowCrystalNum);
+		$("#blueCrystal").attr("data-crystalvalue", blueCrystalNum);
+		$("#greenCrystal").attr("data-crystalvalue", greenCrystalNum);
+		$("#wins").text(wins);
+		$("#losses").text(losses);
+		$("#winOrLose").text("");
+
+	}
+
+	function playerWins() {
+		$("#winOrLose").text("Congrats! You win!");
+		wins++;
+		$("#wins").text(wins);
+	}
+
+	function playerLoses() {
+		$("#winOrLose").text("Sorry! You lose!");
+		losses++;
+		$("#losses").text(losses);
+	}
+
+	newGame();
+
+	$(".crystalimg").hover(function() {
+		$(this).css({opacity: 0.3});
+	},
+	function() {
+		$(this).css({opacity: 1});
+	});
+
+
+	$(".crystalimg").on("click", function() {
+		if (totalScore >= randomNumber) {
+			return;
+		}
+
+		var crystalValue = $(this).attr("data-crystalvalue");
+		crystalValue = parseInt(crystalValue);
+		totalScore += crystalValue;
+		$("#totalScore").text(totalScore);
+
+		if (totalScore === randomNumber) {
+			playerWins();
         } 
-  // Display wins
-  function matched(){
-  alert("Congrats! You won!");
-    wins++; 
-    $("#wins").text(wins);
-    reset();
-  }
-  // Display losses
-  function exceeded(){
-  alert ("Sorry! You lost!");
-    losses++;
-    $("#losses").text(losses);
-    reset()
-  }
-  // Clicking crystals
-    $(".red").on ("click", function(){
-      playerTotal = playerTotal + num1;
-      console.log("New playerTotal= " + playerTotal);
-      $("#totalScore").text(playerTotal); 
-            //Win & lose conditions
-          if (playerTotal == Random){
-            matched();
-          }
-          else (playerTotal > Random){
-            exceeded();
-          }   
-        })  
-
-    $(".blue").on ("click", function(){
-        playerTotal = playerTotal + num2;
-        console.log("New playerTotal= " + playerTotal);
-        $("#totalScore").text(playerTotal); 
-            
-            if (playerTotal == Random){
-                matched();
-            }
-            else (playerTotal > Random){
-                exceeded();
-            } 
-        })  
-
-    $("".yellow").on ("click", function(){
-        playerTotal = playerTotal + num3;
-        console.log("New playerTotal= " + playerTotal);
-        $("#totalScore").text(playerTotal);
-  
-            if (playerTotal == Random){
-            matched();
-            }
-            else (playerTotal > Random){
-            exceeded();
-            } 
-        })  
-
-    $(".green").on ("click", function(){
-        playerTotal = playerTotal + num4;
-        console.log("New playerTotal= " + playerTotal);
-        $("#totalScore").text(playerTotal); 
         
-            if (playerTotal == Random){
-            matched();
-            }
-            else (playerTotal > Random){
-            exceeded();
-            }
-        });   
-    }); 
+        else if (totalScore > randomNumber) {
+			playerLoses();
+		}
+	});
+
+	$(".btn").on("click", function() {
+		newGame();
+	});
+
+});
